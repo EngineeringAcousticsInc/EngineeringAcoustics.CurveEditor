e0021c4449a8e516832ffb5df324341ff77dafb1
nitz <nitz@users.noreply.github.com>
2020-04-24 15:15:00 -0400

    Initial revision.

---
+++ EngineeringAcoustics.CurveControl/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("Library")]
+[assembly: AssemblyTitle("EngineeringAcoustics.Curve")]
+[assembly: AssemblyProduct("EngineeringAcoustics.Curve")]
+[assembly: AssemblyDescription("Curve Editor Control")]
+[assembly: AssemblyCompany("Engineering Acoustics, Inc.")]
+[assembly: AssemblyCopyright("Copyright © Engineering Acoustics, Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cd4b572f-14af-48d8-9291-ef5d30d017b1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # EngineeringAcoustics.CurveEditor
 A fork of the XNA Curve Editor tool that uses the bare minimum of FNA to not depend on the XNA Framework install.
+
+## Not Implemented
+Save/Load is not implemented and will throw a NYI if called.
