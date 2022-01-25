2f755f22376bb76d538bccb570fb946d93519c93
Chris Dailey <nitz@users.noreply.github.com>
2022-01-25 08:33:23 -0500

    Migrate to SDK style projects.

--- EngineeringAcoustics.CurveControl/Properties/AssemblyInfo.cs
+++ EngineeringAcoustics.CurveControl/Properties/AssemblyInfo.cs
@@ -7,13 +7,6 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyConfiguration("Library")]
-[assembly: AssemblyTitle("EngineeringAcoustics.Curve")]
-[assembly: AssemblyProduct("EngineeringAcoustics.Curve")]
-[assembly: AssemblyDescription("Curve Editor Control")]
-[assembly: AssemblyCompany("Engineering Acoustics, Inc.")]
-[assembly: AssemblyCopyright("Copyright © Engineering Acoustics, Inc. 2020")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -22,16 +15,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("cd4b572f-14af-48d8-9291-ef5d30d017b1")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-//[assembly: AssemblyFileVersion("1.0.0.0")]
